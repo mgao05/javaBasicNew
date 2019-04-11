9a9e8c30e3fc225457fc4e4a7b9446690c0d29eb
Mengran <mengrangao@gwu.edu>
2019-04-11 18:37:17 -0400

    add environment variable

---
+++ src/main/java/com/company/environmentVariable/environmentVariable.java
@@ -0,0 +1,8 @@
+package com.company.environmentVariable;
+
+public class environmentVariable {
+
+    public static void main(String[] args){
+        System.out.println(System.getenv("JAVA_HOME"));
+    }
+}
